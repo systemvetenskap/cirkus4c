ea4f1b3a274f7e663f25ad9ea8402ccd6a55670e
dc91 <dc91_@hotmail.com>
2015-09-11 14:46:33 +0200

    tempköp

---
+++ FirstTry/FirstTry/Tempkop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTry
+{
+    class Tempkop
+    {
+        public string forestallning { get; set; }
+        public List<string> akter { get; set; }
+        public int vuxna { get; set; }
+        public int ungdom { get; set; }
+        public int barn { get; set; }
+    }
+}
