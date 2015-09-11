14e29b1feb51dbf4e80da45052eb8dc605610168
dc91 <dc91_@hotmail.com>
2015-09-11 14:42:57 +0200

    biljettköp

--- FirstTry/FirstTry/Biljett.cs
+++ FirstTry/FirstTry/Biljett.cs
@@ -8,5 +8,13 @@ namespace FirstTry
 {
     class Biljett
     {
+
+        public string forestallning { get; set; }
+        public List<string> akter { get; set; }
+        public int pris { get; set; }
+        public DateTime tid { get; set; }
+        public string biljettyp { get; set; }
+        public int biljettnummer { get; set; }
+        
     }
 }
