100f1a70b663efc4c345b74e83342bd60d5b8cf8
dc91 <dc91_@hotmail.com>
2015-09-11 13:19:51 +0200

    Ladda loginrutan

--- FirstTry/FirstTry/Program.cs
+++ FirstTry/FirstTry/Program.cs
@@ -16,7 +16,7 @@ namespace FirstTry
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Loginform());
         }
     }
 }
