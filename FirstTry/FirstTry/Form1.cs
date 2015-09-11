61eb5e961e69fb4cf5bf1cc31276306055fbdd55
dc91 <dc91_@hotmail.com>
2015-09-11 11:30:22 +0200

    gridview

--- FirstTry/FirstTry/Form1.cs
+++ FirstTry/FirstTry/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace FirstTry
 {
@@ -15,6 +16,19 @@ namespace FirstTry
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string sql = "select * from akter";
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            NpgsqlConnection conn = new NpgsqlConnection("Server=webblabb.miun.se;Port=5432;Database=pgmvaru_g4;User Id=pgmvaru_g4;Password=trapets;ssl=true");
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, conn);
+            ds.Reset();
+            da.Fill(ds);
+            dt = ds.Tables[0];
+            dataGridView1.DataSource = dt;
         }
     }
 }
