4e848c6ab280ce18a375bb7299645268fbdb3c9d
dc91 <dc91_@hotmail.com>
2015-09-14 12:29:45 +0200

    Platskarta

--- FirstTry/FirstTry/Platskarta.Designer.cs
+++ FirstTry/FirstTry/Platskarta.Designer.cs
@@ -36,6 +36,12 @@
             this.button_A6 = new System.Windows.Forms.Button();
             this.button_A7 = new System.Windows.Forms.Button();
             this.button_A8 = new System.Windows.Forms.Button();
+            this.radioButton_vuxen = new System.Windows.Forms.RadioButton();
+            this.radioButton_ungdom = new System.Windows.Forms.RadioButton();
+            this.radioButton_barn = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button_A1
@@ -119,11 +125,77 @@
             this.button_A8.Text = "A8";
             this.button_A8.UseVisualStyleBackColor = false;
             // 
+            // radioButton_vuxen
+            // 
+            this.radioButton_vuxen.AutoSize = true;
+            this.radioButton_vuxen.Location = new System.Drawing.Point(13, 48);
+            this.radioButton_vuxen.Name = "radioButton_vuxen";
+            this.radioButton_vuxen.Size = new System.Drawing.Size(55, 17);
+            this.radioButton_vuxen.TabIndex = 8;
+            this.radioButton_vuxen.TabStop = true;
+            this.radioButton_vuxen.Text = "Vuxen";
+            this.radioButton_vuxen.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_ungdom
+            // 
+            this.radioButton_ungdom.AutoSize = true;
+            this.radioButton_ungdom.Location = new System.Drawing.Point(13, 71);
+            this.radioButton_ungdom.Name = "radioButton_ungdom";
+            this.radioButton_ungdom.Size = new System.Drawing.Size(65, 17);
+            this.radioButton_ungdom.TabIndex = 9;
+            this.radioButton_ungdom.TabStop = true;
+            this.radioButton_ungdom.Text = "Ungdom";
+            this.radioButton_ungdom.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_barn
+            // 
+            this.radioButton_barn.AutoSize = true;
+            this.radioButton_barn.Location = new System.Drawing.Point(13, 94);
+            this.radioButton_barn.Name = "radioButton_barn";
+            this.radioButton_barn.Size = new System.Drawing.Size(47, 17);
+            this.radioButton_barn.TabIndex = 10;
+            this.radioButton_barn.TabStop = true;
+            this.radioButton_barn.Text = "Barn";
+            this.radioButton_barn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(84, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "0";
+            // 
             // Platskarta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(861, 474);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton_barn);
+            this.Controls.Add(this.radioButton_ungdom);
+            this.Controls.Add(this.radioButton_vuxen);
             this.Controls.Add(this.button_A8);
             this.Controls.Add(this.button_A7);
             this.Controls.Add(this.button_A6);
@@ -134,7 +206,9 @@
             this.Controls.Add(this.button_A1);
             this.Name = "Platskarta";
             this.Text = "Platskarta";
+            this.Load += new System.EventHandler(this.Platskarta_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -148,5 +222,11 @@
         private System.Windows.Forms.Button button_A6;
         private System.Windows.Forms.Button button_A7;
         private System.Windows.Forms.Button button_A8;
+        private System.Windows.Forms.RadioButton radioButton_vuxen;
+        private System.Windows.Forms.RadioButton radioButton_ungdom;
+        private System.Windows.Forms.RadioButton radioButton_barn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
--- FirstTry/FirstTry/Platskarta.cs
+++ FirstTry/FirstTry/Platskarta.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Npgsql;
 namespace FirstTry
 {
     public partial class Platskarta : Form
@@ -17,10 +17,41 @@ namespace FirstTry
             InitializeComponent();
         }
 
+        NpgsqlConnection conn = new NpgsqlConnection("Server=webblabb.miun.se;Port=5432;Database=pgmvaru_g4;User Id=pgmvaru_g4;Password=trapets;ssl=true");
+        DataTable dt = new DataTable();
+        
+        
+
+
         private void button_A1_Click(object sender, EventArgs e)
         {
             Biljett biljetten = new Biljett();
             
+        }
+
+        private void Platskarta_Load(object sender, EventArgs e)
+        {
+            string query = "select * from tempkop where id = (select MAX(id) from tempkop)";
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(query, conn);
+            da.Fill(dt);
+            Tempkop tk = new Tempkop();
+            foreach (DataRow item in dt.Rows)
+            {
+                int vuxna = Convert.ToInt32(item["vuxna"]);
+                int ungdomar = Convert.ToInt32(item["ungdomar"]);
+                int barn = Convert.ToInt32(item["barn"]);
+                tk.vuxna = vuxna;
+                tk.ungdom = ungdomar;
+                tk.barn = barn;
+
+            }
+          
+            
+
+
+
+
+
         }
     }
 }
