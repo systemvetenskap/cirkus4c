47a83a4fd3e9506a7b27f8afedd75854265ca4a5
dc91 <dc91_@hotmail.com>
2015-09-14 09:34:55 +0200

    forestallning klass

---
+++ FirstTry/FirstTry/Forestallning.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTry
+{
+    class Forestallning
+    {
+        public string namn { get; set; }
+        public int id { get; set; }
+
+
+
+        public override string ToString()
+        {
+            return namn;
+        }
+
+    }
+}
