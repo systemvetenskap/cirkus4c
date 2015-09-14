af8780c1d8b2468fe96f07c8e990dcdf06e0372f
dc91 <dc91_@hotmail.com>
2015-09-14 09:40:05 +0200

    Akt klass

---
+++ FirstTry/FirstTry/Akt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTry
+{
+    class Akt
+    {
+        public string namn { get; set; }
+        public int id { get; set; }
+        public DateTime datumTid { get; set; }
+
+
+        public override string ToString()
+        {
+            return namn;
+        }
+    }
+}
