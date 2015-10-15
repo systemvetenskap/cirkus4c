f4c2ca7b1abe7aa07e4eb7328c248e2f83439257
jili1400 <jill@norten.se>
2015-10-15 11:31:26 +0200

    topp merge

--- FirstTry/FirstTry/Adminhuvudsida.cs
+++ FirstTry/FirstTry/Adminhuvudsida.cs
@@ -44,67 +44,14 @@ namespace FirstTry
 
         private void Adminhuvudsida_Load(object sender, EventArgs e)
         {
-
-            listBoxForestallning.DataSource = Databasmetoder.HamtaForestallningLista();
-
-            /* if (aktorlistaId.Contains(4) == true)
-            {
-                uppdatera.Enabled = true;
-                buttonUppdateraAkt.Enabled = true;
-                textBoxAktVuxenpris.Enabled = true;
-                textBoxAktUngdPris.Enabled = true;
-                TextBoxAktBarnpris.Enabled = true;
-                textBoxVuxenpris.Enabled = true;
-                textBoxUngdomspris.Enabled = true;
-                textBoxBarnpris.Enabled = true;
-                label13.Text = "Sista försäljningsdag \n YYYY-mm-dd HH:mm";
-            }
             if (aktorlistaId.Contains(5) == true || aktorlistaId.Contains(7) == true)
             {
-                this.btnAndraTaBortBeh.Enabled = true;
-                this.btnAndraTaBortBeh.Visible = true;
+                this.buttonAndraBehorighet.Enabled = true;
+                this.buttonAndraBehorighet.Visible = true;
             }
-            if (aktorlistaId.Contains(9) == true)
-            {
-                checkBoxForestallning1.Enabled = true;
-                uppdatera.Enabled = true;
-                buttonUppdateraAkt.Enabled = true;
-            }
+            listBoxForestallning.DataSource = Databasmetoder.HamtaForestallningLista();
 
-            if (aktorlistaId.Contains(3) == true)
-            {
-                //Föreställning edit
-                this.buttonLaggTillForest.Enabled = true;
-                this.buttonLaggTillForest.Visible = true;
-                this.btnSkapaForestallning.Enabled = true;
-                this.btnSkapaForestallning.Visible = true;
-                this.uppdatera.Enabled = true;
-                this.uppdatera.Visible = true;
-                this.buttonTaBort.Enabled = true;
-                this.buttonTaBort.Visible = true;
-                textBoxForestNamn.Enabled = true;
-                richTextBoxForestInf.Enabled = true;
-                textBoxForestDatum1.Enabled = true;
-                textBoxForestStarttid.Enabled = true;
-                textBoxForestSluttid.Enabled = true;
-                textBoxForsaljningsslut.Enabled = true;
-                checkBoxfriPlacering.Enabled = true;
-                checkBoxForestallning1.Enabled = true;
-                //Akt edit
-                this.btnAkt.Enabled = true;
-                this.btnAkt.Visible = true;
-                this.buttonLaggTillAktInfo.Enabled = true;
-                this.buttonLaggTillAktInfo.Visible = true;
-                this.buttonUppdateraAkt.Enabled = true;
-                this.buttonUppdateraAkt.Visible = true;
-                this.button1.Enabled = true;
-                this.button1.Visible = true;
-                textBoxAktnamn.Enabled = true;
-                richTextBoxAktInf.Enabled = true;
-                textBoxAktStarttid.Enabled = true;
-                textBoxAktSluttid.Enabled = true;
-
-            }*/
+          
         }
 
         private void buttonRapporter_Click(object sender, EventArgs e)
